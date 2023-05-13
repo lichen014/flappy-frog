acee6353496ecf1f0538d9bc9076651261631d57
lichen014 <121971822+lichen014@users.noreply.github.com>
2023-05-12 21:58:55 -0700

    finished coding and is currently on UI design

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Vector3 direction;
+    public float gravity = -9.8f;
+    public float strength = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
+            direction = Vector3.up * strength;
+        }
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began) {
+                direction = Vector3.up * strength;
+            }
+        }
+
+        direction.y += gravity * Time.deltaTime;
+        transform.position += direction * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+       if (other.gameObject.tag == "Obstacle") {
+        FindObjectOfType<GameManager>().GameOver();
+       } else if (other.gameObject.tag == "Scoring") {
+        FindObjectOfType<GameManager>().IncreaseScore();
+       }
+    }
+
+}
