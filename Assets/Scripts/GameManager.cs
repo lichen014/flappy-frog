227389f2988a7916b6e898a6fdf5b16e03282038
lichen014 <121971822+lichen014@users.noreply.github.com>
2023-05-14 14:58:42 -0700

    everything working except can't import score text

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-   private int score;
+    public Player player;
+    public Text scoreText;
+    public GameObject playButton;
+    public GameObject gameOver;
+    private int score;
 
-   public void GameOver()
-   {
-        Debug.Log("Game Over");
-   }
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
 
-   public void IncreaseScore()
-   {
+        Pause();
+    }
+
+    public void Play()
+    {
+        score = 0;
+        scoreText.text = score.ToString();
+
+        playButton.SetActive(false);
+        gameOver.SetActive(false);
+
+        Time.timeScale = 1f;
+        player.enabled = true;
+
+        Pipes[] pipes = FindObjectsOfType<Pipes>();
+
+        for (int i = 0; i < pipes.Length; i++) {
+            Destroy(pipes[i].gameObject);
+        }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        player.enabled = false;
+    }
+
+    public void GameOver()
+    {
+        gameOver.SetActive(true);
+        playButton.SetActive(true);
+
+        Pause();
+    }
+
+    public void IncreaseScore()
+    {
         score++;
-   }
+        scoreText.text = score.ToString();
+    }
 }
