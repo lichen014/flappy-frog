6ac94409cd0cea9e623a8af58906160145eef3eb
lichen014 <121971822+lichen014@users.noreply.github.com>
2023-05-14 13:05:52 -0700

    finished parallax script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    private Vector3 direction;
+    public float gravity = -9.8f;
+    public float strength = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,20 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
+            direction = Vector3.up * strength;
+        }
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began) {
+                direction = Vector3.up * strength;
+            }
+        }
+
+        direction.y += gravity * Time.deltaTime;
+        transform.position += direction * Time.deltaTime;
     }
 }
